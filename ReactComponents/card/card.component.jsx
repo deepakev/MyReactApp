aa097b57bf5bbb6b8dd2f90b5425b2801d77903b
deepakev <deepuev@gmail.com>
2020-06-07 07:40:13 +0530

    change test

--- ReactComponents/card/card.component.jsx
+++ ReactComponents/card/card.component.jsx
@@ -7,6 +7,7 @@ export const Card  = props=> (
     <img alt = 'moster' src = {`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
     <h2>{props.monster.name} </h2>
     <p>{props.monster.email}</p>
+    
 </div>
 
 );
